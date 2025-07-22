fdfeeca3e8e5db0a1d050d4543915fbc06b68b72
Ignat <ignatmalouhov@gmail.com>
2025-07-22 11:29:39 +0300

    add swagger

--- module-ai/src/main/java/demo/ai/Test.java
+++ module-ai/src/main/java/demo/ai/Test.java
@@ -3,11 +3,15 @@ package demo.ai;
 import chat.giga.client.GigaChatClient;
 import chat.giga.client.auth.AuthClient;
 import chat.giga.client.auth.AuthClientBuilder;
+import chat.giga.http.client.HttpClientException;
 import chat.giga.model.ModelName;
 import chat.giga.model.Scope;
+import chat.giga.model.TokenCountRequest;
 import chat.giga.model.completion.ChatMessage;
 import chat.giga.model.completion.CompletionRequest;
+import chat.giga.model.embedding.EmbeddingRequest;
 
+import java.util.List;
 import java.util.Optional;
 
 public class Test {
@@ -15,7 +19,8 @@ public class Test {
     static String apiKey = Optional.ofNullable(System.getenv("GIGACHAT_API_KEY"))
             .orElseThrow(() -> new IllegalStateException("GIGACHAT_API_KEY env var is not defined"));
 
-    public static void main(String[] args) {
+
+    static void simpleQuery(String query) {
         GigaChatClient client = GigaChatClient.builder()
                 .authClient(AuthClient.builder()
                         .withOAuth(AuthClientBuilder.OAuthBuilder.builder()
@@ -28,9 +33,57 @@ public class Test {
         System.out.println(client.completions(CompletionRequest.builder()
                 .model(ModelName.GIGA_CHAT)
                 .message(ChatMessage.builder()
-                        .content("Сколько будет два плюс два?")
+                        .content(query)
                         .role(ChatMessage.Role.USER)
                         .build())
                 .build()));
+    }
+
+    static void embeddingQuery(String query) {
+
+        GigaChatClient client = GigaChatClient.builder()
+                .authClient(AuthClient.builder()
+                        .withOAuth(AuthClientBuilder.OAuthBuilder.builder()
+                                .scope(Scope.GIGACHAT_API_PERS)
+                                .authKey(apiKey)
+                                .build())
+                        .build())
+                .build();
+        try {
+            System.out.println(client.embeddings(EmbeddingRequest.builder()
+                    .model("EmbeddingsGigaR")
+                    .input(List.of(query))
+                    .build()));
+        } catch (HttpClientException ex) {
+            System.out.println(ex.statusCode() + " " + ex.bodyAsString());
+        }
+    }
+
+    static void tokens() {
+        GigaChatClient client = GigaChatClient.builder()
+                .authClient(AuthClient.builder()
+                        .withOAuth(AuthClientBuilder.OAuthBuilder.builder()
+                                .scope(Scope.GIGACHAT_API_PERS)
+                                .authKey(apiKey)
+                                .build())
+                        .build())
+                .build();
+        try {
+            System.out.println(client.tokensCount(TokenCountRequest.builder()
+                    .model(ModelName.GIGA_CHAT_MAX)
+                    .input(List.of("Привет, как дела?", "Расскажи о себе"))
+                    .build()));
+        } catch (HttpClientException ex) {
+            System.out.println(ex.statusCode() + " " + ex.bodyAsString());
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+//        simpleQuery("Сколько будет два плюс два?");
+        embeddingQuery("Сколько будет два плюс два?");
+        System.out.println("finish");
+
     }
 }
